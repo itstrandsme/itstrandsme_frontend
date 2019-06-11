ae97fd981275f4df877f52e474d2099c401c4df6
Петр Шургалин <petr.shurgalin@gmail.com>
2019-06-11 16:09:04 +0300

    Project template

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card dark color="primary">
+          <v-card-text class="px-0">Death Stranding fan site</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MainView"
+};
+</script>
