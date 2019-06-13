3c4d7bea2212e9c0fe07ca4bdcfd3aa40140311d
Валера Бохан <bokhan@divo-games.com>
2019-06-13 20:28:02 +0300

    Попытка подключить эффект дождя
    https://github.com/codrops/RainEffect

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,16 +1,10 @@
 <template>
-  <v-container grid-list-md text-xs-center>
-    <v-layout row wrap>
-      <v-flex xs12>
-        <v-card dark color="primary">
-          <v-card-text class="px-0">Death Stranding fan site</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <Rain />
 </template>
 
 <script>
+import Rain from "@/views/Rain.vue"
+
 export default {
   name: "MainView"
 };
---
+++ src/views/Rain.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import 'core-js';
+import RainRenderer from "@/rain/rain-renderer";
+import Raindrops from "@/rain/raindrops";
+import loadImages from "@/rain/image-loader";
+import createCanvas from "@/rain/create-canvas";
+import TweenLite from 'gsap';
+import times from '@/rain/times';
+import {random,chance} from '@/rain/random';
+
+export default {
+  name: "Rain"
+};
+</script>
